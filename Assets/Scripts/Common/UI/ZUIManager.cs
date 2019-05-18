3bf1e180c1f95065b984f85096a270a0c383c396
kongyatong <839339849@qq.com>
2019-05-18 23:37:28 +0800

    ui

---
+++ Assets/Scripts/Common/UI/ZUIManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZUIManager : ZSingletonComp<ZUIManager> {
+
+    public RectTransform rootTrans;
+
+    public void OpenWindow(string name, bool isChildWindow = false) {
+
+    }
+
+
+}
---
+++ Assets/Scripts/Common/UI/ZWindowBase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZWindowBase : ZComponentBase {
+
+    protected ZViewBase view;
+
+    public virtual void SetView(ZViewBase view) {
+        this.view = view;
+    }
+
+    public virtual void OnShow() {
+
+    }
+
+    public virtual void OnClose() {
+
+    }
+
+}
