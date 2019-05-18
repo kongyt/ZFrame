8f3154b889e980ee246a9f4a296bae4e2abe4c2a
kongyatong <839339849@qq.com>
2019-05-18 20:00:39 +0800

    event

---
+++ Assets/Scripts/Common/Events/ZEventDispatcher.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void ZEventListener(ZEventBase evt);
+
+public class ZEventDispatcher {
+
+    private Dictionary<System.Type, ZEventListener> eventHandlers = new Dictionary<System.Type, ZEventListener>();
+
+    public void AddListener(System.Type type, ZEventListener listener) {
+        ZEventListener listeners;
+        if (eventHandlers.TryGetValue(type, out listeners)) {
+            listeners += listener;
+        } else {
+            eventHandlers[type] = listener;
+        }
+    }
+
+    public void RemoveListener(System.Type type, ZEventListener listener) {
+        ZEventListener listeners;
+        if (eventHandlers.TryGetValue(type, out listeners)) {
+            listeners -= listener;
+        } else {
+            eventHandlers.Remove(type);
+        }
+    }
+
+    public void Dispatch(ZEventBase evt) {
+        System.Type type = evt.GetType();
+        ZEventListener listeners;
+        if (eventHandlers.TryGetValue(type, out listeners)) {
+            listeners(evt);
+        }
+    }
+}
---
+++ Assets/Scripts/Common/Events/ZEventManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZEventManager : ZSingleton<ZEventManager> {
+
+    private ZEventDispatcher dispatcher = new ZEventDispatcher();
+
+    public void SendEvent(ZEventBase evt) {
+        dispatcher.Dispatch(evt);
+    }
+
+    public void Listen(System.Type type, ZEventListener listener) {
+        dispatcher.AddListener(type, listener);
+    }
+
+    public void UnListen(System.Type type, ZEventListener listener) {
+        dispatcher.RemoveListener(type, listener);
+    }
+}
